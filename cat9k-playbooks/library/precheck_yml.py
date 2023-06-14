ead6341f495307f89aeae4f617a44a9f8ff98dd1
Andrea Florio <andrea@opensuse.org>
2023-06-14 09:05:19 +0000

    complete underlay validation and simplify vars

--- cat9k-playbooks/library/precheck_yml.py
+++ cat9k-playbooks/library/precheck_yml.py
@@ -12,22 +12,20 @@ import collections
 
 mul_list_dict = collections.defaultdict(list)
 
-def yaml_error_validation(parsed_underlay, parsed_overlay, debug) :
+def yaml_underlay_validation(parsed_underlay) :
+
     """
     validate some error scenarios in the yaml file
     Args:
         parsed_underlay: underlay yaml file converted to dict
-        parsed_overlay: overlay yaml file converted to dict
-        debug: for completed output 
     Returns:
         string : "partial validation for vlan and svi is done successfully"
     Raises:
         KeyError if key not founds
-    Error_senarios :
-        vni duplication,evi duplication,svi_type core duplication
+        ValueError if values are not of the correct type
     """
 
-    # check if all mandatory sections are present in the underlay
+    # Validate that the STP section exists and has the correct values
     if "stp" not in parsed_underlay :
         raise KeyError ("Mandatory stp section is missig in the underlay_db")
     if "priority" not in parsed_underlay['stp'] :
@@ -37,27 +35,29 @@ def yaml_error_validation(parsed_underlay, parsed_overlay, debug) :
         if parsed_underlay['stp']['priority'] not in valid_priorities :
             raise ValueError (f"Configure STP priority is not a valid value.\n supported values are: {', '.join(valid_priorities)}")
 
-
-    if "domain" not in parsed_underlay :
-        raise KeyError ("Mandatory domain section is missig in the underlay_db")
-    if "name" not in parsed_underlay['domain'] :
-        raise KeyError ("Mandatory domain.name section is missig in the underlay_db")
+    # Validate that the domain_name exists and has the correct values
+    if "domain_name" not in parsed_underlay :
+        raise KeyError ("Mandatory domain_name section is missig in the underlay_db")
 
 
+    # Validate that the Underlay section exists and has the correct values
     if "underlay" not in parsed_underlay:
         raise KeyError ("Mandatory underlay section is missig in the underlay_db")
+    
     if "router_id" not in parsed_underlay['underlay']:
         raise KeyError ("Mandatory underlay.router_id section is missig in the underlay_db")
     elif not parsed_underlay['underlay']['router_id'].startswith('Loopback') :
         raise ValueError ("underlay.router_id must be a Loopback interface")
+    
     if "vtep_name" not in parsed_underlay['underlay']:
         raise KeyError ("Mandatory underlay.vtep_name section is missig in the underlay_db")
     elif not parsed_underlay['underlay']['vtep_name'].startswith('Loopback') :
         raise ValueError ("underlay.vtep_name must be a Loopback interface")
+    
     if parsed_underlay['underlay']['router_id'] == parsed_underlay['underlay']['vtep_name'] :
         raise ValueError ("underlay.router_id and underlay.vtep_name must be different Loopbacks")
 
-
+    # Validate that the OSPF section exists and has the correct values
     if "ospf" not in parsed_underlay:
         raise KeyError ("Mandatory ospf section is missig in the underlay_db") 
     if "area" not in parsed_underlay['ospf']:
@@ -68,7 +68,7 @@ def yaml_error_validation(parsed_underlay, parsed_overlay, debug) :
         except :
             raise ValueError ("OSPF area must be in dotted format")
 
-
+    # Validate that the BGP section exists and has the correct values
     if "bgp" not in parsed_underlay:
         raise KeyError ("Mandatory bgp section is missig in the underlay_db")
     if "as_number" not in parsed_underlay['bgp']:
@@ -92,16 +92,20 @@ def yaml_error_validation(parsed_underlay, parsed_overlay, debug) :
                 ipaddress.IPv4Address(spine_ip)
             except :
                 raise ValueError ("BGP Spine IP must be a valid IPv4 address")
+            if ipaddress.IPv4Address(spine_ip) not in ipaddress.IPv4Network(parsed_underlay['bgp']['leafs_range']) :
+                raise ValueError ("BGP Spine IPs must be in the same network as bgp.leafs_range") 
         spines = parsed_underlay['bgp']['spines']
         unique_spines = set(spines)
         if len(spines) != len(unique_spines) or not (2 <= len(spines) <= 4):
             raise ValueError ("BGP Spine IPs must be different and must be at least 2 and max 4")
 
+    return ("Underlay YAML Validation done successfully")
 
+def yaml_overlay_validation(parsed_overlay) :
     # check if all mandatory sections are present in the underlay
-    if "anycastgateway" not in parsed_overlay:
-        raise KeyError ("Mandatory anycastgateway section is missig in the underlay_db")
-    
+    if "anycastgateway_mac" not in parsed_overlay:
+        raise KeyError ("Mandatory anycastgateway_mac section is missig in the underlay_db")
+        
     if "vrfs" not in parsed_overlay:
         raise KeyError ("Mandatory vrfs section is missig in the underlay_db")
 
@@ -110,10 +114,10 @@ def yaml_error_validation(parsed_underlay, parsed_overlay, debug) :
 
     return ("partial validation for underlay and overlay is done successfully")
     
-def vlan_svi_validation(parsed_overlay, debug) :
+def vlan_svi_validation(parsed_overlay) :
     return ("partial validation for vlan and svi is done successfully")
     
-def vrf_validation(parsed_overlay, debug) :
+def vrf_validation(parsed_overlay) :
     return ("partial validation for VRFs is done successfully")
 
         
@@ -121,8 +125,7 @@ def run_module():
     module = AnsibleModule(
         argument_spec=dict( 
           underlay_db=dict(type='dict', required=True),
-          overlay_db=dict(type='dict', required=True),
-          debug=dict(type='str', required=True)
+          overlay_db=dict(type='dict', required=True)
         ),
         supports_check_mode=True
     )   
@@ -131,9 +134,10 @@ def run_module():
 
     #result['precheck'] = parsed_yaml
     result['yaml_precheck']= ( 
-        yaml_error_validation(module.params['underlay_db'], module.params['overlay_db'], module.params['debug']),
-        vlan_svi_validation(module.params['overlay_db'], module.params['debug']),
-        vrf_validation(module.params['overlay_db'], module.params['debug']),
+        yaml_underlay_validation(module.params['underlay_db']),
+        yaml_overlay_validation(module.params['overlay_db']),
+        vlan_svi_validation(module.params['overlay_db']),
+        vrf_validation(module.params['overlay_db']),
     )    
     module.exit_json(**result)
 
